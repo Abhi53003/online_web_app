a1021918f765c3bb8441741d9fb23db520b4df96
Abhi53003 <abhiweb29@gmail.com>
2024-07-21 07:41:13 +0000

    add array list and add product data

--- info.js
+++ info.js
@@ -1,3 +1,92 @@
+
+// create a array for list of items we have
+const products=[
+  {
+    id:1,
+    title:"Air Force",
+    price:119,
+    colors:[
+      {
+      code:"black",
+      img:"./img/air.png",
+      },
+      {
+        code:"darkblue",
+        img:"./img/air2.png",
+          
+      },
+    ],
+  },
+  {
+    id:2,
+    title:"Air Jordan",
+    price:149,
+    colors:[
+      {
+      code:"light grey",
+      img:"./img/jordan.png",
+      },
+      {
+        code:"green",
+        img:"./img/jordan2.png",
+          
+      },
+    ],
+  },
+  {
+    id:3,
+    title:"blazer",
+    price:109,
+    colors:[
+      {
+      code:"lightgrey",
+      img:"./img/blazer.png",
+      },
+      {
+        code:"green",
+        img:"./img/blazer2.png",
+          
+      },
+    ],
+  },
+  {
+    id:4,
+    title:"crater",
+    price:125,
+    colors:[
+      {
+      code:"lightgrey",
+      img:"./img/crater.png",
+      },
+      {
+        code:"green",
+        img:"./img/crater2.png",
+          
+      },
+    ],
+  },
+  {
+    id:5,
+    title:"hippie",
+    price:146,
+    colors:[
+      {
+      code:"grey",
+      img:"./img/crater.png",
+      },
+      {
+        code:"orange",
+        img:"./img/crater2.png",
+          
+      },
+    ],
+  },
+
+
+
+]
+
+
 // Select the wrapper element
 const wrapper = document.querySelector(".sliderWrapper");
 
@@ -14,3 +103,6 @@ menuItems.forEach((item, index) => {
 
 // Initial transform value (optional)
 wrapper.style.transform = "translateX(-300vw)";
+
+// creating a array for customization and apply effect 
+
